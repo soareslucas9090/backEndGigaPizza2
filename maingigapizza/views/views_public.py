4e51c0bd2c005e694cddc85d746c9830dcd11904
Lucas Soares <soareslukas9090@hotmail.com>
2024-07-10 03:25:29 -0300

    fix: tentativa 2

--- maingigapizza/views/views_public.py
+++ maingigapizza/views/views_public.py
@@ -1,4 +1,3 @@
-import subprocess
 from pathlib import Path
 
 from django.shortcuts import render
@@ -38,16 +37,6 @@ class ListSalablesViewSet(GenericViewSet, ListModelMixin):
     http_method_names = ["get"]
 
     def list(self, request, *args, **kwargs):
-        result = subprocess.run(
-            "cd.. && ls", capture_output=True, text=True, shell=True
-        )
-
-        if result.returncode == 0:
-            # Imprime a saída do comando
-            print("Arquivos e pastas:")
-            print(result.stdout)
-        else:
-            # Em caso de erro, imprime a mensagem de erro
-            print("Erro ao executar o comando 'ls':")
-            print(result.stderr)
+        print("===== Aqui")
+        print(Path(__file__).resolve().parent.parent.parent)
         return super().list(request, *args, **kwargs)
