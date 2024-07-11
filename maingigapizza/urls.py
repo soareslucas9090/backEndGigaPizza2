80895ea847e09d8aefeb907482989051c7c857b0
Lucas Soares <soareslukas9090@hotmail.com>
2024-07-11 02:47:46 -0300

    feat: atualização de basenames de endpoints

--- maingigapizza/urls.py
+++ maingigapizza/urls.py
@@ -11,15 +11,25 @@ from .views.views_admin import (
 from .views.views_public import CreateUserViewSet, ListSalablesViewSet
 
 gigapizza_router_admin = SimpleRouter()
-gigapizza_router_admin.register("categorys", CategorysViewSet)
-gigapizza_router_admin.register("subcategorys", SubCategorysViewSet)
-gigapizza_router_admin.register("inputs", InputsViewSet)
-gigapizza_router_admin.register("salables", SalablesViewSet)
-gigapizza_router_admin.register("inputs_salables", InputsSalablesViewSet)
+gigapizza_router_admin.register(
+    "categorys", CategorysViewSet, basename="admin-categorys"
+)
+gigapizza_router_admin.register(
+    "subcategorys", SubCategorysViewSet, basename="admin-subcategorys"
+)
+gigapizza_router_admin.register("inputs", InputsViewSet, basename="admin-inputs")
+gigapizza_router_admin.register("salables", SalablesViewSet, basename="admin-salables")
+gigapizza_router_admin.register(
+    "inputs_salables", InputsSalablesViewSet, basename="admin-inputs_salables"
+)
 
 gigapizza_router_public = SimpleRouter()
-gigapizza_router_public.register("create_user", CreateUserViewSet)
-gigapizza_router_public.register("list_salables", ListSalablesViewSet)
+gigapizza_router_public.register(
+    "create_user", CreateUserViewSet, basename="public-create_user"
+)
+gigapizza_router_public.register(
+    "list_salables", ListSalablesViewSet, basename="public-list_salables"
+)
 
 urlpatterns = [
     path("v1/admin/", include(gigapizza_router_admin.urls)),
