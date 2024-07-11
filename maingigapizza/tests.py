96df1190dafee64fc6a07a387d89f4e3c68580ef
Lucas Soares <soareslukas9090@hotmail.com>
2024-07-11 02:58:53 -0300

    test: testes completos de auth, public.create_user e admin.categorys

--- maingigapizza/tests.py
+++ maingigapizza/tests.py
@@ -1,3 +1,108 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from maingigapizza.models import Categorys, Users
+
+
+class AdminRoutersAPITestCase(APITestCase):
+    #####################################################
+    #####################################################
+    ############## Auth & Public.CreateUser #############
+    #####################################################
+    #####################################################
+    def setUp(self):
+        # Credenciais que serão usadas para teste
+        self.email_user = "testuser@example.com"
+        self.pass_user = "password123"
+
+        # Criação de Usuário
+        self.user_data = {
+            "name": "testuser",
+            "email": self.email_user,
+            "cpf": "12345678910",
+            "tel": "9999999999",
+            "password": self.pass_user,
+        }
+        self.create_user_url = reverse("public-create_user-list")
+        response = self.client.post(self.create_user_url, self.user_data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # Tranformação de usuário em Admin para os testes posteriores
+        self.user = Users.objects.get(email=self.email_user)
+        self.user.is_admin = True
+        self.user.save()
+
+        # Obtenção de token jwt para autenticação nos testes posteriores
+        login_url = reverse("token_obtain_pair")
+        login_data = {"email": self.email_user, "password": self.pass_user}
+        login_response = self.client.post(login_url, login_data, format="json")
+        self.assertEqual(login_response.status_code, status.HTTP_200_OK)
+
+        # Autenticação de usuário com o response obtido
+        self.access_token = login_response.data["access"]
+        self.client.credentials(HTTP_AUTHORIZATION="Bearer " + self.access_token)
+
+    #####################################################
+    #####################################################
+    ################## Admin.Categorys ##################
+    #####################################################
+    #####################################################
+
+    def test_create_category(self):
+        # Dados da nova categoria
+        category_url = reverse("admin-categorys-list")
+        category_data = {"name": "New Category"}
+
+        # Teste de status HTTP
+        response = self.client.post(category_url, category_data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # Teste de pesquisa direta no banco
+        category = Categorys.objects.get(name="New Category")
+        self.assertEqual(category.name, "New Category")
+        self.assertTrue(category.is_active)
+
+    def test_get_category(self):
+        # Teste de resposta sem nenhum registro
+        category_url = reverse("admin-categorys-list")
+        response = self.client.get(category_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["results"], [])
+
+        # Teste de resposta com um registro
+        new_category = {"name": "New Category"}
+        self.client.post(category_url, new_category, format="json")
+        response = self.client.get(category_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            response.data["results"],
+            [{"id": 1, "name": "New Category", "is_active": True}],
+        )
+
+    def test_patch_category(self):
+        # Inclusão de dado para teste posterior
+        category_url = reverse("admin-categorys-list")
+        new_category = {"name": "New Category"}
+        self.client.post(category_url, new_category, format="json")
+
+        # Teste de atualização de um campo
+        category_url = reverse("admin-categorys-detail", kwargs={"pk": 1})
+        new_data = {"name": "Updated Category"}
+        response = self.client.patch(category_url, new_data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            response.data,
+            {"id": 1, "name": "Updated Category", "is_active": True},
+        )
+
+    def test_delete_category(self):
+        # Inclusão de dado para teste posterior
+        category_url = reverse("admin-categorys-list")
+        new_category = {"name": "New Category"}
+        self.client.post(category_url, new_category, format="json")
+
+        # Teste de exclusão de um registro
+        category_url = reverse("admin-categorys-detail", kwargs={"pk": 1})
+        response = self.client.delete(category_url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
