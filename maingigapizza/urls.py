887bd33cc324dba57293893c4394328275d7ec59
Lucas Soares <soareslukas9090@hotmail.com>
2024-07-14 15:43:06 -0300

    fix: notação raw para evitar problemas nas strings

--- maingigapizza/urls.py
+++ maingigapizza/urls.py
@@ -12,26 +12,26 @@ from .views.views_public import CreateUserViewSet, ListSalablesViewSet
 
 gigapizza_router_admin = SimpleRouter()
 gigapizza_router_admin.register(
-    "categorys", CategorysViewSet, basename="admin-categorys"
+    r"categorys", CategorysViewSet, basename="admin-categorys"
 )
 gigapizza_router_admin.register(
-    "subcategorys", SubCategorysViewSet, basename="admin-subcategorys"
+    r"subcategorys", SubCategorysViewSet, basename="admin-subcategorys"
 )
-gigapizza_router_admin.register("inputs", InputsViewSet, basename="admin-inputs")
-gigapizza_router_admin.register("salables", SalablesViewSet, basename="admin-salables")
+gigapizza_router_admin.register(r"inputs", InputsViewSet, basename="admin-inputs")
+gigapizza_router_admin.register(r"salables", SalablesViewSet, basename="admin-salables")
 gigapizza_router_admin.register(
-    "inputs_salables", InputsSalablesViewSet, basename="admin-inputs_salables"
+    r"inputs_salables", InputsSalablesViewSet, basename="admin-inputs_salables"
 )
 
 gigapizza_router_public = SimpleRouter()
 gigapizza_router_public.register(
-    "create_user", CreateUserViewSet, basename="public-create_user"
+    r"create_user", CreateUserViewSet, basename="public-create_user"
 )
 gigapizza_router_public.register(
-    "list_salables", ListSalablesViewSet, basename="public-list_salables"
+    r"list_salables", ListSalablesViewSet, basename="public-list_salables"
 )
 
 urlpatterns = [
-    path("v1/admin/", include(gigapizza_router_admin.urls)),
-    path("v1/public/", include(gigapizza_router_public.urls)),
+    path(r"v1/admin/", include(gigapizza_router_admin.urls)),
+    path(r"v1/public/", include(gigapizza_router_public.urls)),
 ]
